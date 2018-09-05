ef55f3bfcf4881f63d30b7ee0f56c084934bcc5d
CarJem Generations <cwallacecs@gmail.com>
2018-09-04 23:21:53 -0400

    Finally Fix Incorrect Bridge Rendering

--- ManiacEditor/EditorEntity.cs
+++ ManiacEditor/EditorEntity.cs
@@ -682,15 +682,18 @@ namespace ManiacEditor
             }
             else if (entity.Object.Name.Name == "Bridge")
             {
-                var value = entity.attributesMap["length"].ValueUInt8 / 2;
+                var value = entity.attributesMap["length"].ValueUInt8;
                 var editorAnim = LoadAnimation2("Bridge", d, 0, 0, false, false, false);
                 if (editorAnim != null && editorAnim.Frames.Count != 0)
                 {
                     var frame = editorAnim.Frames[0];
-                    for (int i = -value; i < value + 1; ++i)
+                    bool wEven = value % 2 == 0;
+                    for (int xx = 0; xx <= value; ++xx)
                     {
-                        d.DrawBitmap(frame.Texture, x + frame.Frame.CenterX + (i * (frame.Frame.Width + 2)),
-                            y + frame.Frame.CenterY, frame.Frame.Width, frame.Frame.Height, false, Transparency);
+                        d.DrawBitmap(frame.Texture,
+                            x + (wEven ? frame.Frame.CenterX : -frame.Frame.Width) + (-value / 2 + xx) * frame.Frame.Width,
+                            y + frame.Frame.CenterY,
+                            frame.Frame.Width, frame.Frame.Height, false, Transparency);
                     }
                 }
             }
